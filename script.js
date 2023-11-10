ab626e0c34ceb34bc0c6bbd22ce26f173828414b
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-10 20:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  let ans = "#";
+  let curr = 0;
+  if (shortHex.startsWith("#")) {
+    curr = 1;
+  }
+
+  for (let i = curr; curr < shortHex.length; curr++) {
+    ans += shortHex.charAt(curr).toLowerCase();
+    ans += shortHex.charAt(curr).toLowerCase();
+  }
+
+  return ans;
 };
 
-// Do not change the code below.
+// Do not change the codebelow.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
